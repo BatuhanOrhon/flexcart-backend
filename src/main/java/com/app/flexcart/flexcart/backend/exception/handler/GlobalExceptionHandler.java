d50abdfca37bb13afcc023c8bc6bc90239aaaed2
BatuhanOrhon <batuorhon@windowslive.com>
2025-05-13 19:58:57 +0300

    ExceptionHandler is fixed.

--- src/main/java/com/app/flexcart/flexcart/backend/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/app/flexcart/flexcart/backend/exception/handler/GlobalExceptionHandler.java
@@ -2,8 +2,8 @@ package com.app.flexcart.flexcart.backend.exception.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.app.flexcart.flexcart.backend.exception.ActionFactoryException;
 import com.app.flexcart.flexcart.backend.exception.ActionFactoryParameterCannotBeNullException;
@@ -16,49 +16,49 @@ import com.app.flexcart.flexcart.backend.exception.ProductNotFoundException;
 import com.app.flexcart.flexcart.backend.exception.UserNotFoundException;
 import com.app.flexcart.flexcart.backend.exception.dto.ErrorResponse;
 
-@RestController
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({
-            ActionFactoryException.class,
-            ConditionFactoryException.class,
-            InvalidParametersFormatException.class
-    })
-    public ResponseEntity<ErrorResponse> handleBadRequest(RuntimeException ex) {
-        ErrorResponse body = new ErrorResponse(
-                ex.getMessage(),
-                HttpStatus.BAD_REQUEST.value());
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(body);
-    }
+        @ExceptionHandler({
+                        ActionFactoryException.class,
+                        ConditionFactoryException.class,
+                        InvalidParametersFormatException.class
+        })
+        public ResponseEntity<ErrorResponse> handleBadRequest(RuntimeException ex) {
+                ErrorResponse body = new ErrorResponse(
+                                ex.getMessage(),
+                                HttpStatus.BAD_REQUEST.value());
+                return ResponseEntity
+                                .status(HttpStatus.BAD_REQUEST)
+                                .body(body);
+        }
 
-    @ExceptionHandler({
-            ActionFactoryParameterCannotBeNullException.class,
-            ConditionFactoryParameterCannotBeNullException.class
-    })
-    public ResponseEntity<ErrorResponse> handleUnprocessableEntity(RuntimeException ex) {
-        ErrorResponse body = new ErrorResponse(
-                ex.getMessage(),
-                HttpStatus.UNPROCESSABLE_ENTITY.value());
-        return ResponseEntity
-                .status(HttpStatus.UNPROCESSABLE_ENTITY)
-                .body(body);
-    }
+        @ExceptionHandler({
+                        ActionFactoryParameterCannotBeNullException.class,
+                        ConditionFactoryParameterCannotBeNullException.class
+        })
+        public ResponseEntity<ErrorResponse> handleUnprocessableEntity(RuntimeException ex) {
+                ErrorResponse body = new ErrorResponse(
+                                ex.getMessage(),
+                                HttpStatus.UNPROCESSABLE_ENTITY.value());
+                return ResponseEntity
+                                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                                .body(body);
+        }
 
-    @ExceptionHandler({
-            CartNotFoundException.class,
-            ProductNotFoundException.class,
-            UserNotFoundException.class,
-            CampaignNotFoundException.class,
-    })
-    public ResponseEntity<ErrorResponse> handleNotFound(RuntimeException ex) {
-        ErrorResponse body = new ErrorResponse(
-                ex.getMessage(),
-                HttpStatus.NOT_FOUND.value());
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(body);
-    }
+        @ExceptionHandler({
+                        CartNotFoundException.class,
+                        ProductNotFoundException.class,
+                        UserNotFoundException.class,
+                        CampaignNotFoundException.class,
+        })
+        public ResponseEntity<ErrorResponse> handleNotFound(RuntimeException ex) {
+                ErrorResponse body = new ErrorResponse(
+                                ex.getMessage(),
+                                HttpStatus.NOT_FOUND.value());
+                return ResponseEntity
+                                .status(HttpStatus.NOT_FOUND)
+                                .body(body);
+        }
 
 }
